cd3263c20c737434235f9d6bbec77f0607a4a024
sugimoto0925 <hidetora.ryusuke@gmail.com>
2024-06-17 00:55:30 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,10 @@
-//変数の宣言・値の代入
-let BMI = (68 / [1.7 * 1.7]);
+//体重と身長を変数に代入
+let weight = 68;
+let height = 1.7;
+
+//BMIを計算
+let BMI = weight / (height * height);
+
 
 //コンソールへの出力
 console.log(BMI)
