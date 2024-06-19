b79624e224c8a60ef727bf60b42a4771d4a2855d
sugimoto0925 <hidetora.ryusuke@gmail.com>
2024-06-19 23:04:53 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,13 @@
+// 変数(num)に1以上の正の数を代入
+let num = 10;
+
+// 条件分岐
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+    console.log("3の倍数です");
+} else if (num % 5 === 0) {
+    console.log("5の倍数です");
+} else {
+    console.log(num);
+}
