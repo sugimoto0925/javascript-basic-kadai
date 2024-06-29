fe2ee6263622dc9faf20ec82842cf74b92bb795e
sugimoto0925 <hidetora.ryusuke@gmail.com>
2024-06-29 23:20:51 +0900

    Add files via upload

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,7 +1,7 @@
 // 引数で渡した値を二乗した計算結果を戻り値として返す関数
-const atai = (square) => {
-  console.log(square * square);
+const double = (num) => {
+  return num * num;
 }
 
-// 引数
-atai(10);
+// 関数の戻り値を出力する
+console.log(double(10));
